c694907c3ad578fab3c953c5d487b14820d58cd3
Pratham Gowtham <pratham.gowtham@gmail.com>
2024-01-10 19:47:33 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class main {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.print();
+        list.addLast(20);
+        list.print();
+        list.addLast(30);
+        list.print();
+    }
+
+}
